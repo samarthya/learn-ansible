da0c7375c59ec589868efd384443be7243a8e76c
Samarthya Saurabh <saurabh777@gmail.com>
2022-02-25 16:31:08 +0530

    Removed others

--- dump.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS test (
-    message VARCHAR(255) NOT NULL
-)ENGINE=MyISAM DEFAULT CHARSET=utf8;
-INSERT INTO test(message) values('Ansible to do list');
-INSERT INTO test(message) values('Get ready');
-INSERT INTO test(message) values('Ansible is fun');
